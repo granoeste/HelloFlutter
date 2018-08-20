0986723417fceb4ae03d5f41455a7c308629b558
granoeste <granoeste@gmail.com>
2018-08-20 16:19:16 +0900

    modify cirrus-ci config

--- build_android.sh
+++ build_android.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-
 set -e
 
-flutter doctor
+flutter doctor -v
 
-echo "$GOOGLE_PLAY_UPLOAD_KEY" | base64 --decode > android/fake_upload.jks
-echo "$GOOGLE_PLAY_UPLOAD_KEY_PROPERTIES" | base64 --decode > android/key.properties
-ls -ls android
+# echo "$GOOGLE_PLAY_UPLOAD_KEY" | base64 --decode > android/fake_upload.jks
+# echo "$GOOGLE_PLAY_UPLOAD_KEY_PROPERTIES" | base64 --decode > android/key.properties
+# ls -ls android
 
-flutter build apk --release
+# flutter build apk --release
 
 cd android
 fastlane version
-fastlane update_fastlane
+# fastlane update_fastlane
 
-fastlane deploy_play_store
+# fastlane deploy_play_store
