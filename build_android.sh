ef154930ca6fdb8a9f19444707b3beda71b1139d
granoeste <granoeste@gmail.com>
2018-08-24 14:25:34 +0900

    modify cirrus-ci config

--- build_android.sh
+++ build_android.sh
@@ -5,7 +5,9 @@ flutter doctor -v
 
 # Build preconfig
 if [ $CI ]; then
-    echo "Build on CI"
+    echo "┌─────────────────────┐"
+    echo "│     Build on CI     │"
+    echo "└─────────────────────┘"
     echo "$GOOGLE_PLAY_UPLOAD_KEY" | base64 --decode > android/fake_upload.jks
     echo "$GOOGLE_PLAY_UPLOAD_KEY_PROPERTIES" | base64 --decode > android/key.properties
     echo sdk.dir="$ANDROID_HOME" > android/local.properties
@@ -21,9 +23,9 @@ ls -ls android
 cd android
 
 # Update fastlane
-fastlane -v
-sudo fastlane update_fastlane
+# bundle exec fastlane -v
+# sudo fastlane update_fastlane
 
 # Build and Deploy Play Store
-# fastlane build_newer_and_deploy_play_store
-fastlane retrieve_newer_track_version_code
+# bundle exec fastlane build_newer_and_deploy_play_store
+bundle exec fastlane retrieve_newer_track_version_code
