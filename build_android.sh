9381a3ae90326f6c7a7217f0428a80ad8541f119
granoeste <granoeste@gmail.com>
2018-08-27 17:56:56 +0900

    modify cirrus-ci config

--- build_android.sh
+++ build_android.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 
-flutter doctor -v
-
 # Build preconfig
 if [ $CI ]; then
     echo "┌─────────────────────┐"
     echo "│     Build on CI     │"
     echo "└─────────────────────┘"
+    flutter doctor -v
     echo "Build preconfiguration..."
     echo "$GOOGLE_PLAY_UPLOAD_KEY" | base64 --decode > android/fake_upload.jks
     echo "$GOOGLE_PLAY_UPLOAD_KEY_PROPERTIES" | base64 --decode > android/key.properties
@@ -18,8 +17,6 @@ fi
 
 # Build and Deploy Play Store
 cd android
-bundle -v
-bundle list
 # bundle exec fastlane build_newer_and_deploy_play_store
 # bundle exec fastlane retrieve_newer_track_version_code
 # bundle exec fastlane build_newer
--- build_ios.sh
+++ build_ios.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-flutter doctor -v
-
 # Build preconfig
 if [ $CI ]; then
     echo "┌─────────────────────┐"
     echo "│     Build on CI     │"
     echo "└─────────────────────┘"
     echo "Build preconfiguration..."
+    git clone -b beta https://github.com/flutter/flutter.git
+    export PATH=`pwd`/flutter/bin:$PATH
+    flutter doctor -v
 fi
 
 # Build and Deploy Play Store
