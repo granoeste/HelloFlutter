b685254fd086d0ff676d071b01e4268bea415445
granoeste <granoeste@gmail.com>
2018-08-28 14:48:22 +0900

    modify cirrus-ci config

--- build_ios.sh
+++ build_ios.sh
@@ -6,12 +6,24 @@ if [ $CI ]; then
     echo "┌─────────────────────┐"
     echo "│     Build on CI     │"
     echo "└─────────────────────┘"
-    echo "Build preconfiguration..."
+    echo "Build preconfiguration ..."
+    echo "Define ssh config ..."
+    [ ! -e ~/.ssh ] && mkdir -p ~/.ssh
+    echo "$SSH_CONFIG" | base64 --decode > ~/.ssh/config
+    echo "$GIT_PRIVATE_KEY" | base64 --decode > ~/.ssh/github
+    echo "Install Flutter ..."
     git clone -b beta https://github.com/flutter/flutter.git
     export PATH=`pwd`/flutter/bin:$PATH
+    echo "Install iOS toolchain ..."
+    brew install --HEAD libimobiledevice
+    brew install ideviceinstaller
+    brew install ios-deploy
+
     flutter doctor -v
 fi
 
 # Build and Deploy Play Store
 cd ios
+bundle exec fastlane sync_code_signing
+security find-identity -v -p codesigning
 bundle exec fastlane build
